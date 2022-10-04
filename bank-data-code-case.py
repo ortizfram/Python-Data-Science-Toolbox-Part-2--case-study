2b549e75cbdfc9afed2c178388a8307dc5b02876
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-04 12:20:19 -0800

    Update bank-data-code-case.py

--- bank-data-code-case.py
+++ bank-data-code-case.py
@@ -43,3 +43,43 @@ print(rs_fxn)
 """ output
 {'CountryName': 'Arab World', 'CountryCode': 'ARB', 'IndicatorName': 'Adolescent fertility rate (births per 1,000 women ages 15-19)', 'IndicatorCode': 'SP.ADO.TFRT', 'Year': '1960', 'Value': '133.56090740552298'}
 """
+#-----------------------------------------------------------------------------------#
+#Using a list comprehension
+"""
+This time, you're going to use the lists2dict() function you defined in the last exercise to turn a bunch of lists into a list of dictionaries with the help of a list comprehension.
+
+The lists2dict() function has already been preloaded, together with a couple of lists, feature_names and row_lists. feature_names contains the header names of the World Bank dataset and row_lists is a list of lists, where each sublist is a list of actual values of a row from the dataset.
+
+Your goal is to use a list comprehension to generate a list of dicts, where the keys are the header names and the values are the row entries.
+
+--Instructions
+100 XP
+Inspect the contents of row_lists by printing the first two lists in row_lists.
+Create a list comprehension that generates a dictionary using lists2dict() for each sublist in row_lists. The keys are from the feature_names list and the values are the row entries in row_lists. Use sublist as your iterator variable and assign the resulting list of dictionaries to list_of_dicts.
+Look at the first two dictionaries in list_of_dicts by printing them out."""
+
+# Print the first two lists in row_lists
+print(row_lists[0])
+print(row_lists[1])
+
+# Turn list of lists into list of dicts: list_of_dicts
+list_of_dicts = [lists2dict(feature_names, sublist) for sublist in row_lists]
+
+# Print the first two dictionaries in list_of_dicts
+print(list_of_dicts[0])
+print(list_of_dicts[1])
+#-----------------------------------------------------------------------------------#
+#Turning this all into a DataFrame
+"""You've zipped lists together, created a function to house your code, and even used the function in a list comprehension to generate a list of dictionaries. That was a lot of work and you did a great job!
+
+You will now use all of these to convert the list of dictionaries into a pandas DataFrame. You will see how convenient it is to generate a DataFrame from dictionaries with the DataFrame() function from the pandas package.
+
+The lists2dict() function, feature_names list, and row_lists list have been preloaded for this exercise.
+
+Go for it!
+
+Instructions
+100 XP
+To use the DataFrame() function you need, first import the pandas package with the alias pd.
+Create a DataFrame from the list of dictionaries in list_of_dicts by calling pd.DataFrame(). Assign the resulting DataFrame to df.
+Inspect the contents of df printing the head of the DataFrame. Head of the DataFrame df can be accessed by calling df.head()."""
