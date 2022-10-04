de0bcdf3d0f0d8eddfdf05126636fe6ddd955600
Franco Ortiz <51888893+ortizfram@users.noreply.github.com>
2022-10-04 13:42:32 -0800

    Update bank-data-code-case.py

--- bank-data-code-case.py
+++ bank-data-code-case.py
@@ -298,8 +298,9 @@ print(df_urb_pop.head())
 df_pop_ceb = df_urb_pop[df_urb_pop['CountryCode'] == 'CEB']
 
 # Zip DataFrame columns of interest: pops
-pops = zip("Total Population", "Urban population (% of total)")
-
+pops = zip(df_pop_ceb['Total Population'], 
+            df_pop_ceb['Urban population (% of total)'])
+            
 # Turn zip object into list: pops_list
 pops_list = list(pops)
 
